3f59c294f99d068358b294caf9b6dbf919591754
dli267@wisc.edu <Quartermoon2>
2020-03-02 15:59:33 -0600

    finished

---
+++ app/src/main/java/c/sakshi/lab5/Main2Activity.java
@@ -0,0 +1,103 @@
+package c.sakshi.lab5;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class Main2Activity extends AppCompatActivity {
+
+    public static ArrayList<Note> notes;
+    TextView welcomeMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        welcomeMessage = (TextView) findViewById(R.id.welcomeMessage);
+        Intent intent = getIntent();
+        String str = intent.getStringExtra("username");
+        welcomeMessage.setText("Welcome " + str + "!");
+
+        Context context = getApplicationContext();
+        SQLiteDatabase sqLiteDatabase = context.openOrCreateDatabase("notes", Context.MODE_PRIVATE, null);
+        DBHelper dbHelper = new DBHelper(sqLiteDatabase);
+        notes = dbHelper.readNotes(str);
+
+
+
+        //SQLiteDatabase sqLiteDatabase;
+        //DBHelper dbHelper = new DBHelper(sqLiteDatabase);
+
+
+        ArrayList<String> displayNotes = new ArrayList<>();
+        for (Note note : notes) {
+            displayNotes.add(String.format("Title:%s\nDate:%s", note.getTitle(), note.getDate()));
+
+        }
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, displayNotes);
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), Note.class);
+                intent.putExtra("noteid", position);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.item1:
+                //Toast.makeText(this, "Item 1 selected", Toast.LENGTH_SHORT).show();
+                goToActivity3();
+                return true;
+            case R.id.item2:
+                SharedPreferences sharedPreferences = getSharedPreferences("c.sakshi.lab5", Context.MODE_PRIVATE);
+                sharedPreferences.edit().remove("username").apply();
+                goToActivity1();
+                return true;
+
+            default: return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void goToActivity1() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToActivity3() {
+        Intent intent = new Intent(this, Main3Activity.class);
+        startActivity(intent);
+    }
+}
